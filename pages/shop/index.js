7abaa122e6c9ad3a89d99c6be9119b4d315a2d6f
LoDaChing <chingching@LoDaChingdeMacBook-Pro.local>
2021-06-08 15:43:38 +0800

    chang shoppage api url

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -2,19 +2,19 @@ import ShopContainer from '../../components/category/ShopContainer'
 
 export async function getServerSideProps() {
 
-    const response = await fetch(`https://dry-wave-60207.herokuapp.com/api/shop`)
-    const items = await response.json()
+  const response = await fetch(`https://dachingapi.herokuapp.com/Products`)
+  const items = await response.json()
 
-    return {
-        props: {
-            items
-        }
+  return {
+    props: {
+      items
     }
+  }
 }
 
 export default function JacketsPage({ items }) {
   return (
-    <ShopContainer categories={items} title = {'Shop'}/>
+    <ShopContainer categories={items} title={'Shop'} />
 
   )
 }
