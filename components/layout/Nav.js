5c70d52db131019a81c2a278f9b9db129773cc9d
LoDaChing <chingching@LoDaChingdeMacBook-Pro.local>
2021-06-08 16:48:49 +0800

    use react-bootstrap dropdown

--- components/layout/Nav.js
+++ components/layout/Nav.js
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import styles from './nav.module.scss'
+import { Dropdown } from 'react-bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Nav() {
   return (
@@ -23,16 +25,20 @@ export default function Nav() {
             </li>
 
             <li className={styles.nav__listItem}>
-              Category
-                        <ul className={styles.nav__listItemDrop}>
-                <li><Link href="/shop/hats">Hats</Link></li>
-                <li><Link href="/shop/jackets">Jackets</Link></li>
-                <li><Link href="/shop/sneakers">Sneakers</Link></li>
-                <li><Link href="/shop/womens">Women's</Link></li>
-                <li><Link href="/shop/mens">Men's</Link></li>
-              </ul>
-            </li>
+              <Dropdown className={styles.nav__listItemDrop}>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Category
+                </Dropdown.Toggle>
 
+                <Dropdown.Menu>
+                  <Dropdown.Item href="/shop/hats">Hats</Dropdown.Item>
+                  <Dropdown.Item href="/shop/jackets">Jackets</Dropdown.Item>
+                  <Dropdown.Item href="/shop/sneakers">Sneakers</Dropdown.Item>
+                  <Dropdown.Item href="/shop/womens">Women's</Dropdown.Item>
+                  <Dropdown.Item href="/shop/mens">Men's</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </li>
           </ul>
         </nav>
       </div>
